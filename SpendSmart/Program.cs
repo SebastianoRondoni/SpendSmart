6f776dee4554f8f21de689365cc652968f8dd460
Sebastiano Rondoni <sebastianorondoni@gmail.com>
2025-09-05 15:49:59 +0200

    fix: add check for table existence and creation

--- SpendSmart/Program.cs
+++ SpendSmart/Program.cs
@@ -30,6 +30,12 @@ namespace SpendSmart
 
             var app = builder.Build();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<SpendSmartDbContext>();
+                context.Database.EnsureCreated();
+            }
+
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
